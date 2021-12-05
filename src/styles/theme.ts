660c33180f351e4a7a228be6e5efa15d3577e201
galdinoramon <ramon.galdino@atmansystems.com>
2021-12-05 00:53:42 -0300

    Criando tema

---
+++ src/styles/theme.ts
@@ -0,0 +1,27 @@
+const theme = {
+    colors: {
+        white: '#FFFFFF',
+
+        grey_lightest: '#F9F9F9',
+        grey_light: '#E1E1E1',
+        grey_medium: '#B5B5B5',
+        grey_dark: '#505050',
+        grey_darkest: '#252525',
+
+        primary_lightest: '#BDACEF',
+        primary_light: '#9775FA',
+        primary_medium: '#845EF7',
+        primary_dark: '#7950F2',
+
+        success: '#51CF66',
+        warning: '#FCC419',
+        error: '#FF6B6B',
+    },
+
+    metrics: {
+        base: 8,
+        page_padding: 24,
+    },
+};
+
+export default theme;
